1a1b179e90451dbc2e745a783158b38e59a9787a
yaroslav_holovatskyy <jaroslav.holovatskii@gmail.com>
2024-05-25 21:29:36 -0500

    changes

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6551686511c2aa433626ae7d48274e6de388695")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d62e61630ef6fc29df70b0ac16f0bebc3381105b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
