d62e61630ef6fc29df70b0ac16f0bebc3381105b
yaroslav_holovatskyy <jaroslav.holovatskii@gmail.com>
2024-05-25 21:13:18 -0500

    End of section 3

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14cfc3e7000c654d2013ab8eebe7c0329062a9c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6551686511c2aa433626ae7d48274e6de388695")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
